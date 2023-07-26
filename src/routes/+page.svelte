4a5d5b7c7b337187f4266a0b8d94b89134a0dd4b
Jinzo <artesgo@gmail.com>
2023-07-26 18:50:59 -0400

    cookie click

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let cookie = 0;
+    function rnjesus() {
+        cookie++;
+    }
+</script>
+
+<div> Cookies: {cookie} </div>
+
+artesgo
+
+<button class="cookie" on:click={() => rnjesus()}> Cookie </button>
+<button> Grandma </button>
+<button> Mine </button>
+
+<style>
+    .cookie {
+        border: 1px solid;
+        border-radius: 50%;
+        height: 50px;
+        width: 50px;
+    }
+</style>
